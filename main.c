ecd673d4f2868ef098f53d27d0bd2f4278945b67
Unknown <ricardo.s.fernandes@tecnico.ulisboa.pt>
2019-05-10 17:40:16 +0100

    second line output

--- main.c
+++ main.c
@@ -62,6 +62,8 @@ int pairStation(graph* G, int station) {
 
 int isStation(graph* G, int i) { return i >= i_sStation(G, 0); }
 
+int vIndex(vertex* v0, vertex* vi) { return vi - v0; }
+
 void parse_input(graph* G) {
     int nSuppliers, nStations, nEdges;
     scanf("%d %d %d", &nSuppliers, &nStations, &nEdges);
@@ -72,36 +74,43 @@ void parse_input(graph* G) {
     G->E = (edge*)malloc(sizeof(edge) * G->nE);
     G->nSup = nSuppliers;
 
-    int s_f = 0, i_edge;
-    G->V[0] = new_vertex(0, G->nV);
-    G->V[1] = new_vertex(0, 0);
+    G->V[0] = new_vertex(0, 0);
+    G->V[1] = new_vertex(0, G->nV);
+    int i_edge, v_c;
     for (i_edge = 0; i_edge < nSuppliers; i_edge++) {
-        int v_c;
         scanf("%d", &v_c);
-        G->V[i_edge + 2] = new_vertex(v_c, 0);
-        G->E[i_edge] = new_edge(&G->V[0], &G->V[i_edge + 2], v_c, v_c);
-        s_f += v_c;
+        G->V[i_edge + 2] = new_vertex(0, 0);
+        G->E[i_edge] = new_edge(&G->V[i_edge + 2], &G->V[0], 0, v_c);
     }
-    G->V[0].e = -s_f;
 
+    int s, f;
     for (int i = 0; i < nStations; i++) {
-        int s = i_sStation(G, i), f = i_fStation(G, i);
+        s = i_sStation(G, i);
+        f = i_fStation(G, i);
         G->V[s] = G->V[f] = new_vertex(0, 0);
-        int v_c;
         scanf("%d", &v_c);
         G->E[i_edge++] = new_edge(&G->V[s], &G->V[f], 0, v_c);
     }
 
+    int eo, ed, ec, flow, s_f = 0;
     for (int i = 0; i < nEdges; i++) {
-        int eo, ed, ec;
+        flow = 0;
         scanf("%d %d %d", &eo, &ed, &ec);
-        if (isStation(G, eo)) {
+
+        if (ed == 1) {
+            flow = ec;
+            G->V[eo].e = flow;
+            s_f -= flow;
+        }
+
+        if (isStation(G, ed)) {
             G->E[i_edge++] =
-                new_edge(&G->V[pairStation(G, eo)], &G->V[ed], 0, ec);
+                new_edge(&G->V[pairStation(G, ed)], &G->V[eo], flow, ec);
         } else {
-            G->E[i_edge++] = new_edge(&G->V[eo], &G->V[ed], 0, ec);
+            G->E[i_edge++] = new_edge(&G->V[ed], &G->V[eo], flow, ec);
         }
     }
+    G->V[1].e = s_f;
 }
 
 int isBackEdge(edge* e, vertex* v) { return e->d == v; }
@@ -159,9 +168,13 @@ void push_relabel(graph* G) {
     int q_len = G->nE * G->nV;
     edge** v_edges = (edge**)malloc(sizeof(edge*) * G->nE);
     vertex** Q = (vertex**)malloc(sizeof(vertex*) * q_len);
-    for (int i = 0; i < G->nSup; i++) Q[i] = &G->V[i + 2];
+    for (int i = 0; i < q_len; i++) Q[i] = NULL;
 
-    int q_index = 0, nQ = G->nSup;
+    int nQ = 0;
+    for (int i = 0; i < G->nE; i++)
+        if (G->E[i].o == &G->V[1]) Q[nQ++] = G->E[i].d;
+
+    int q_index = 0;
     while (Q[q_index] != NULL) {
         while (Q[q_index]->e > 0) {
             find_edges_av(v_edges, G, Q[q_index]);
@@ -172,12 +185,12 @@ void push_relabel(graph* G) {
                 if (isBackEdge(v_edges[edge_index], Q[q_index])) {
                     if (Q[q_index]->h > v_edges[edge_index]->o->h) {
                         push(v_edges[edge_index], Q[q_index]);
-                        ins_q(Q, &q_len, &nQ, v_edges[edge_index]->o, &G->V[0]);
+                        ins_q(Q, &q_len, &nQ, v_edges[edge_index]->o, &G->V[1]);
                     }
                 } else {
                     if (Q[q_index]->h > v_edges[edge_index]->d->h) {
                         push(v_edges[edge_index], Q[q_index]);
-                        ins_q(Q, &q_len, &nQ, v_edges[edge_index]->d, &G->V[1]);
+                        ins_q(Q, &q_len, &nQ, v_edges[edge_index]->d, &G->V[0]);
                     }
                 }
                 edge_index++;
@@ -189,36 +202,26 @@ void push_relabel(graph* G) {
     free(Q);
 }
 
-void print_node(graph* G, node* n) {
-    for (node* i = n; i != NULL; i = i->next)
-        printf("%ld ", ((vertex*)i->data) - G->V);
+void free_nodes(node* head) {
+    if (head == NULL) return;
+    free_nodes(head->next);
+    free(head);
 }
-
-void print_hash(graph* G, node** h, int max) {
-    for (int i = 0; i < max; i++) {
-        printf("\n%d: ", i);
-        print_node(G, h[i]);
-    }
-}
-
-void print_edge(vertex* startPtr, edge* e) {
-    printf("{ %ld:(%d,%d) -> %ld:(%d,%d), %d/%d} \n", e->o - startPtr, e->o->h,
-           e->o->e, e->d - startPtr, e->d->h, e->d->e, e->c, e->f);
-}
-
-void print_graph(graph* G) {
-    printf("\nGRAPH (V, E)(nSuppliers) = (%d, %d)(%d):\n", G->nV, G->nE,
-           G->nSup);
-    for (int i = 0; i < G->nE; i++) print_edge(G->V, &G->E[i]);
-}
+/*
+void free_cut(node* h) {
+    if (h == NULL) return;
+    free_cut(h->next);
+    free((int**)h->data)
+}*/
 
 void parse_output(graph* G) {
     // FIRST LINE OUTPUT : MAXIMUM FLOW
-    printf("%d\n", -G->V[0].e);
+    printf("%d\n", G->V[0].e);
 
     // DEALING WITH LAST LINES OUTPUT : EDGES TO BE RAISED
     int max_h = 2 * G->nV;
     node** h_list = (node**)malloc(sizeof(node*) * max_h);
+    for (int i = 0; i < max_h; i++) h_list[i] = NULL;
 
     for (int i = 0; i < G->nV; i++) {
         if (i + 1 >= max_h) {
@@ -246,6 +249,10 @@ void parse_output(graph* G) {
              i_node = i_node->next)
             s_side = new_node(i_node->data, s_side);
 
+    // Clean unused hashtable:
+    for (int i = 0; i < max_h; i++) free_nodes(h_list[i]);
+    free(h_list);
+
     node* cut = NULL;
     for (int i = 0; i < G->nE; i++) {
         int flag_so = 0;
@@ -259,25 +266,41 @@ void parse_output(graph* G) {
         if (flag_so)  // origin of edge is in the s-side
             for (node* i_node = t_side; i_node != NULL; i_node = i_node->next) {
                 if (G->E[i].d == i_node->data) {
-                    cut = new_node(&G->E[i], cut);
+                    int** iV_edge = (int**)malloc(sizeof(int*));
+                    *iV_edge = (int*)malloc(sizeof(int) * 2);
+                    (*iV_edge)[0] = vIndex(G->V, G->E[i].o);
+                    (*iV_edge)[1] = vIndex(G->V, G->E[i].d);
+                    cut = new_node(iV_edge, cut);
                     break;
                 }
             }
         else  // origin of edge is in the t-side
             for (node* i_node = s_side; i_node != NULL; i_node = i_node->next) {
                 if (G->E[i].d == i_node->data) {
-                    cut = new_node(&G->E[i], cut);
+                    int** iV_edge = (int**)malloc(sizeof(int*));
+                    *iV_edge = (int*)malloc(sizeof(int) * 2);
+                    (*iV_edge)[0] = vIndex(G->V, G->E[i].o);
+                    (*iV_edge)[1] = vIndex(G->V, G->E[i].d);
+                    cut = new_node(iV_edge, cut);
                     break;
                 }
             }
     }
+    free_nodes(s_side);
+    free_nodes(t_side);
 
     // Having the cut edges, we just need to figure out which are whom
 
-    for (node* i_node = cut; i_node != NULL; i_node = i_node->next)
-        if ((((edge*)i_node->data)->o - G->V) >= (G->nSup + 2))
-            printf("%ld", ((edge*)i_node->data)->o - G->V);
+    for (node* i_node = cut; i_node != NULL; i_node = i_node->next) {
+        if ((*(int**)i_node->data)[0] >= (G->nSup + 2) &&
+            (*(int**)i_node->data)[1] >= (G->nSup + 2)) {
+            printf("%d ", (*(int**)i_node->data)[0]);
+        }
+    }
     printf("\n");
+
+    // free_cut(cut);
+    free_nodes(cut);
 }
 
 void free_graph(graph* G) {
@@ -295,10 +318,57 @@ int main() {
     free_graph(G);
 }
 
-/**
- * printf(
-            "fSent = min(eC-eF, vE) = min(%d, %d) = %d ::: (0ld, New):\n\tvE = "
+/*
+printf(
+            "fSent = min(eC-eF, vE) = min(%d, %d) = %d ::: (0ld, New):\n\tvE
+ = "
             "(%d, %d)\n\teF = (%d, %d)\n",
             e->c - e->f, v->e, fSent, e->o->e, e->o->e + fSent, e->f,
             e->f - fSent);
- */
+
+PRINT CUT EDGES NODE:
+    for (node* i_node = cut; i_node != NULL; i_node = i_node->next) {
+        printf("(%d->%d) -> ", (*(int**)i_node->data)[0],
+               (*(int**)i_node->data)[1]);
+    }
+
+PRINT SIDES OF CUT:
+    printf("\nprint \'s_side\': ");
+    print_vertex_node(G, s_side);
+    printf("print \'t_side\': ");
+    print_vertex_node(G, t_side);
+    printf("\n");
+
+
+void print_edge(vertex* startPtr, edge* e) {
+    printf("{ %ld:(%d,%d) -> %ld:(%d,%d), %d/%d } \n", e->o - startPtr, e->o->h,
+           e->o->e, e->d - startPtr, e->d->h, e->d->e, e->c, e->f);
+}
+
+void print_graph(graph* G) {
+    printf("\nGRAPH (V, E)(nSuppliers) = (%d, %d)(%d):\n", G->nV, G->nE,
+           G->nSup);
+    for (int i = 0; i < G->nE; i++) print_edge(G->V, &G->E[i]);
+}
+
+
+void print_vertex_node(graph* G, node* n) {
+    for (node* i = n; i != NULL; i = i->next)
+        printf("%d -> ", vIndex(G->V, (vertex*)i->data));
+    printf("\n");
+}
+
+void print_edge_node(graph* G, node* n) {
+    printf("\nprint_edge_node:\n");
+    for (node* i = n; i != NULL; i = i->next) print_edge(G->V, i->data);
+}
+
+void print_hash(graph* G, node** h, int max) {
+    for (int i = 0; i < max; i++) {
+        printf("%d: ", i);
+        print_vertex_node(G, h[i]);
+    }
+}
+
+
+*/
